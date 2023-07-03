db709d45f65240abdf8de8a8f9a7d3d3013cf370
Evgeny Shestak <107277013+Karavang@users.noreply.github.com>
2023-07-03 15:37:47 +0200

    don't workable reg

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,23 +1,33 @@
 import List from './ContactList';
 import Filter from './Filter';
 import Form from './Form';
+import Logmodal from './Logmodal';
+
+const showlog = true;
 
 function Phonebook() {
   return (
-    <div className="all-pos">
-      <ul className="flexator g-20">
-        <li>
-          <h2>Phonebook</h2>
-          <Form />
-        </li>
+    <>
+      <Logmodal />
+      {showlog ? (
+        <div></div>
+      ) : (
+        <div className="all-pos">
+          <ul className="flexator g-20">
+            <li>
+              <h2>Phonebook</h2>
+              <Form />
+            </li>
 
-        <li className="li-contacts">
-          <h2>Contacts</h2>
-          <Filter />
-          <List />
-        </li>
-      </ul>
-    </div>
+            <li className="li-contacts">
+              <h2>Contacts</h2>
+              <Filter />
+              <List />
+            </li>
+          </ul>
+        </div>
+      )}
+    </>
   );
 }
 
---
+++ src/components/Logmodal.jsx
@@ -0,0 +1,56 @@
+import React from 'react';
+
+export default function Logmodal() {
+  return (
+    <div className="modal-log">
+      <ul className="nav nav-tabs flexator" id="myTab" role="tablist">
+        <li className="nav-item" role="presentation">
+          <button
+            className="nav-link active"
+            id="home-tab"
+            data-bs-toggle="tab"
+            data-bs-target="#home"
+            type="button"
+            role="tab"
+            aria-controls="home"
+            aria-selected="true"
+          >
+            Login
+          </button>
+        </li>
+        <li className="nav-item" role="presentation">
+          <button
+            className="nav-link"
+            id="profile-tab"
+            data-bs-toggle="tab"
+            data-bs-target="#profile"
+            type="button"
+            role="tab"
+            aria-controls="profile"
+            aria-selected="false"
+          >
+            Registration
+          </button>
+        </li>
+      </ul>
+      <div className="tab-content" id="myTabContent">
+        <div
+          className="tab-pane fade show active"
+          id="home"
+          role="tabpanel"
+          aria-labelledby="home-tab"
+        >
+          ...
+        </div>
+        <div
+          className="tab-pane fade"
+          id="profile"
+          role="tabpanel"
+          aria-labelledby="profile-tab"
+        >
+          ...
+        </div>
+      </div>
+    </div>
+  );
+}
