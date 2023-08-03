2cdea3d43139e235f0a3b2c0e304aecd639a8887
Evgeny Shestak <107277013+Karavang@users.noreply.github.com>
2023-08-03 13:58:57 +0200

    fini

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,30 @@
 import { useSelector } from 'react-redux';
-import { Navigate } from 'react-router-dom';
+import { Navigate, useNavigate } from 'react-router-dom';
 import { getIsLogged, getUser } from 'redux/operations/regs';
 import Logmodal from './Logmodal';
+import { useEffect, useState } from 'react';
 function Phonebook() {
   const showlog = useSelector(getIsLogged);
   const user = useSelector(getUser);
   console.log(user);
+  const navigate = useNavigate();
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    // Здесь вы можете выполнить любые действия, которые необходимо сделать после начала загрузки страницы
+
+    // Пример: Проверка условия и перенаправление на /login, если нужно
+    if (isLoading) {
+      // Установите условие в соответствии с вашими требованиями
+      const isLoggedIn = false; // Здесь должна быть логика проверки авторизации пользователя
+
+      if (!isLoggedIn) {
+        navigate('/login');
+      }
+
+      setIsLoading(false);
+    }
+  }, [isLoading, navigate]);
   return <>{showlog ? <Navigate to="/contacts" /> : <Logmodal />}</>;
 }
 
